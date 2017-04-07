0b9982e18a20e6eed8260c21502a9afe12be9782
demerino <code@getbraintree.com>
2017-04-07 10:50:24 -0700

    Rename BTDropInBaseViewControllerTests tests

--- UnitTests/BTDropInBaseViewControllerTests.m
+++ UnitTests/BTDropInBaseViewControllerTests.m
@@ -7,22 +7,12 @@
 
 @implementation BTDropInBaseViewControllerTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)test_prefersStatusBarHidden_returnsTrue {
+- (void)testPrefersStatusBarHidden_returnsTrue {
     BTDropInBaseViewController *viewController = [[BTDropInBaseViewController alloc] init];
     XCTAssertTrue(viewController.prefersStatusBarHidden);
 }
 
-- (void)test_preferredStatusBarUpdateAnimation_returns_UIStatusBarAnimationSlide {
+- (void)testPreferredStatusBarUpdateAnimation_returnsUIStatusBarAnimationSlide {
     BTDropInBaseViewController *viewController = [[BTDropInBaseViewController alloc] init];
     XCTAssertEqual(UIStatusBarAnimationSlide, viewController.preferredStatusBarUpdateAnimation);
 }
