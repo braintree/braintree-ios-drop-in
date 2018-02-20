afa08e7b73626759fe33ad582ae497d792510d62
demerino <code@getbraintree.com>
2018-02-20 10:40:33 -0800

    Enable secureTextEntry for BTUIKSecurityCodeFormField

--- BraintreeUIKit/Components/BTUIKSecurityCodeFormField.m
+++ BraintreeUIKit/Components/BTUIKSecurityCodeFormField.m
@@ -16,6 +16,7 @@
         self.formLabel.text = BTUIKLocalizedString(SECURITY_CODE_LABEL);
         self.textField.placeholder = BTUIKLocalizedString(CVV_FIELD_PLACEHOLDER);
         self.textField.keyboardType = UIKeyboardTypeNumberPad;
+        self.textField.secureTextEntry = YES;
     }
     return self;
 }
