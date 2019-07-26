70281c3174e0764cd11a5ebdd8728d42afb6cbdd
sestevens <51723734+sestevens@users.noreply.github.com>
2019-07-26 10:47:21 -0500

    Remove Target SDK Checks (#163)
    * Removed unnecessary pre-processor directives checking for appropriate iOS version.
    * Update CHANGELOG.md
    
    Co-authored-by: scannillo <sammyjaynecannillo@gmail.com>
    Co-authored-by: sestevens <susan.stevens@getbraintree.com>

--- DropInDemo/Demo Base/BraintreeDemoAppDelegate.m
+++ DropInDemo/Demo Base/BraintreeDemoAppDelegate.m
@@ -20,25 +20,12 @@ NSString *BraintreeDemoAppDelegatePaymentsURLScheme = @"com.braintreepayments.Dr
     return YES;
 }
 
-#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 90000
 - (BOOL)application:(__unused UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options {
     if ([[url.scheme lowercaseString] isEqualToString:[BraintreeDemoAppDelegatePaymentsURLScheme lowercaseString]]) {
         return [BTAppSwitch handleOpenURL:url options:options];
     }
     return YES;
 }
-#endif
-
-// Deprecated in iOS 9, but necessary to support < versions
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-implementations"
-- (BOOL)application:(__unused UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(__unused id)annotation {
-    if ([[url.scheme lowercaseString] isEqualToString:[BraintreeDemoAppDelegatePaymentsURLScheme lowercaseString]]) {
-        return [BTAppSwitch handleOpenURL:url sourceApplication:sourceApplication];
-    }
-    return YES;
-}
-#pragma clang diagnostic pop
 
 - (void)setupAppearance {
     UIColor *pleasantGray = [UIColor colorWithWhite:42/255.0f alpha:1.0f];
--- DropInDemo/Features/Drop In/BraintreeDemoDropInViewController.m
+++ DropInDemo/Features/Drop In/BraintreeDemoDropInViewController.m
@@ -305,7 +305,6 @@
     }
 }
 
-#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 110000
 - (void)paymentAuthorizationViewController:(__unused PKPaymentAuthorizationViewController *)controller didAuthorizePayment:(PKPayment *)payment handler:(void (^)(PKPaymentAuthorizationResult * _Nonnull))completion API_AVAILABLE(ios(11.0), watchos(4.0)) {
     self.progressBlock(@"Apple Pay Did Authorize Payment");
     BTAPIClient *client = [[BTAPIClient alloc] initWithAuthorization:self.authorizationString];
@@ -320,7 +319,6 @@
         }
     }];
 }
-#endif
 
 - (void)paymentAuthorizationViewController:(__unused PKPaymentAuthorizationViewController *)controller
                        didAuthorizePayment:(PKPayment *)payment
