ec0aba8eed1ccc6293d7e33315ffb5df49610c54
scannillo <samantha.cannillo@getbraintree.com>
2020-11-06 07:52:20 -0600

    wip

---
+++ .github/workflows/test_app.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+set -o pipefail && xcodebuild -workspace 'BraintreeDropIn.xcworkspace' -sdk 'iphonesimulator' -configuration 'Release' -scheme 'UnitTests' -destination 'name=iPhone 11,platform=iOS Simulator'  test  | ../.././Pods/xcbeautify/xcbeautify
