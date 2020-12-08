880cd079bea6772789270bb9d2c30875f061edd5
Samantha Cannillo <samantha.cannillo@getbraintree.com>
2020-12-08 11:52:39 -0600

    Add Package.swift file
    Signed-off-by: Susan Stevens <susan.stevens@getbraintree.com>

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BraintreeDropIn",
+    platforms: [.iOS(.v12)],
+    products: [
+//        .library(
+//            name: "BraintreeDropIn",
+//            targets: ["BraintreeDropIn"]
+//        ),
+        .library(
+            name: "BraintreeUIKit",
+            targets: ["BraintreeUIKit"]
+        )
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // .target(
+        //     name: "BraintreeAmericanExpress",
+        //     dependencies: ["BraintreeCore"],
+        //     exclude: ["Info.plist"],
+        //     publicHeadersPath: "Public"
+        // ),
+        .target(
+            name: "BraintreeUIKit",
+            exclude: ["Info.plist"],
+            publicHeadersPath: "Public"
+        )
+    ]
+)
