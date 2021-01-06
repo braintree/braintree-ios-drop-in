3118f1875f52996d52b6806736205c0b3a87d5a6
Susan Stevens <susan.stevens@getbraintree.com>
2021-01-06 10:55:08 -0600

    Use master branch of braintree_ios in Package.swift

--- Package.swift
+++ Package.swift
@@ -18,8 +18,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        //.package(name: "Braintree", url: "https://github.com/braintree/braintree_ios.git", from: "5.0.0-beta1")
-        .package(name: "Braintree", path: "/Users/scannillo/bt/braintree-ios")
+        .package(name: "Braintree", url: "https://github.com/braintree/braintree_ios.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
