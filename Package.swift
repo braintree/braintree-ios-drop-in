8217ed6607a68b7fdec5e6a2bf531f0f1bfc982e
Samantha Cannillo <samantha.cannillo@getbraintree.com>
2021-01-06 14:26:16 -0600

    Remove .git from end of url in Package.swift
    Signed-off-by: Susan Stevens <susan.stevens@getbraintree.com>

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "Braintree", url: "https://github.com/braintree/braintree_ios.git", .branch("master"))
+        .package(name: "Braintree", url: "https://github.com/braintree/braintree_ios", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
