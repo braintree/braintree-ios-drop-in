e9bf14956947a1bccaba049df4817bc158aa0ccf
Susan Stevens <susan.stevens@getbraintree.com>
2021-01-14 17:47:56 -0600

    Restore SPMTest sample app

---
+++ SampleApps/SPMTest/SPMTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPMTest
+//
+//  Created by Cannillo, Sammy on 12/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
