b631d20581a6e77caf892a09f22b93ff05d3d68f
Susan Stevens <susan.stevens@getbraintree.com>
2021-04-02 16:50:30 -0500

    Add UI tests for Venmo

---
+++ Demo/UITests/BraintreeDropIn_Venmo_UITests.swift
@@ -0,0 +1,54 @@
+import XCTest
+
+class Venmo_UITests: XCTestCase {
+
+    var demoApp: XCUIApplication!
+    var mockVenmo: XCUIApplication!
+
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+
+        mockVenmo = XCUIApplication(bundleIdentifier: "com.braintreepayments.MockVenmo")
+        mockVenmo.activate()
+
+        demoApp = XCUIApplication(bundleIdentifier: "com.braintreepayments.DropInDemo")
+        demoApp.launchArguments.append("-EnvironmentSandbox")
+        demoApp.launchArguments.append("-ClientToken")
+        demoApp.launch()
+
+        waitForElementToAppear(demoApp.buttons["Add Payment Method"])
+        demoApp.buttons["Add Payment Method"].tap()
+    }
+
+    func testTokenizeVenmo_whenAuthorizationSuccessful_returnsNonce() {
+        waitForElementToBeHittable(demoApp.staticTexts["Venmo"])
+        demoApp.staticTexts["Venmo"].tap()
+
+        waitForElementToAppear(mockVenmo.buttons["SUCCESS"])
+        mockVenmo.buttons["SUCCESS"].tap()
+
+        waitForElementToAppear(demoApp.staticTexts["venmojoe"])
+    }
+
+    func testTokenizeVenmo_whenErrorOccurs_returnsError() {
+        waitForElementToBeHittable(demoApp.staticTexts["Venmo"])
+        demoApp.staticTexts["Venmo"].tap()
+
+        waitForElementToAppear(mockVenmo.buttons["ERROR"])
+        mockVenmo.buttons["ERROR"].tap()
+
+        waitForElementToAppear(demoApp.staticTexts["Select Payment Method"])
+    }
+
+    func testTokenizeVenmo_whenUserCancels_returnsCancel() {
+        waitForElementToBeHittable(demoApp.staticTexts["Venmo"])
+        demoApp.staticTexts["Venmo"].tap()
+
+        waitForElementToAppear(mockVenmo.buttons["Cancel"])
+        mockVenmo.buttons["Cancel"].tap()
+
+        waitForElementToAppear(demoApp.staticTexts["Select Payment Method"])
+    }
+}
+
