2257b4d7b77575e3b6e31d6f602be11f950efdad
scannillo <35243507+scannillo@users.noreply.github.com>
2021-11-03 15:56:38 -0500

    Add Apple Silicon Support (#357)
    - Update Podspec & Package.swift to require `braintree_ios` 5.5.0+
    - Remove `EXCLUDED_ARCHS = arm64` workarounds, which are no longer required
    - Update to latest GH CI Xcode images where possible

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "Braintree", url: "https://github.com/braintree/braintree_ios", from: "5.4.3")
+        .package(name: "Braintree", url: "https://github.com/braintree/braintree_ios", from: "5.5.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
