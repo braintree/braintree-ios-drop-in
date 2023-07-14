c7d9648b58342b14d35037b8a73fbea67bcc16c2
Jax DesMarais-Leder <jdesmarais@paypal.com>
2023-07-14 15:57:29 -0500

    WIP - update imports

--- Sources/BraintreeDropIn/Public/BraintreeDropIn/BraintreeDropIn.h
+++ Sources/BraintreeDropIn/Public/BraintreeDropIn/BraintreeDropIn.h
@@ -8,9 +8,8 @@ FOUNDATION_EXPORT const unsigned char BraintreeDropInVersionString[];
 
 #if __has_include(<Braintree/Braintree-Swift.h>) // CocoaPods
 #import <Braintree/Braintree-Swift.h>
-//#else
-//@import BraintreeVenmo;
-//@import BraintreeApplePay;
+#else
+// TODO: what to do here?
 #endif
 
 #import <BraintreeDropIn/BTDropInController.h>
