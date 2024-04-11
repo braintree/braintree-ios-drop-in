a91448897ac7d3f00db23d487eda863f9571c67e
Victoria Park <vicpark@paypal.com>
2024-04-11 08:06:52 -0700

    update podspec and Package.swift 5.25.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "Braintree", url: "https://github.com/braintree/braintree_ios", from: "5.23.0")
+        .package(name: "Braintree", url: "https://github.com/braintree/braintree_ios", from: "5.25.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
