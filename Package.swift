b3c4dba89a914b0b0cc999b25d4139f16dd8c4bf
stechiu <stechiu@paypal.com>
2025-07-30 14:16:01 -0700

    Resolved Package error

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "Braintree", url: "https://github.com/braintree/braintree_ios", from: "5.26.0")
+        .package(url: "https://github.com/braintree/braintree_ios", from: "5.26.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -22,15 +22,15 @@ let package = Package(
         .target(
             name: "BraintreeDropIn",
             dependencies: [
-                .product(name: "BraintreeApplePay", package: "Braintree"),
-                .product(name: "BraintreeCard", package: "Braintree"),
-                .product(name: "BraintreeCore", package: "Braintree"),
-                .product(name: "BraintreePaymentFlow", package: "Braintree"),
-                .product(name: "BraintreePayPal", package: "Braintree"),
-                .product(name: "BraintreeThreeDSecure", package: "Braintree"),
-                .product(name: "BraintreeUnionPay", package: "Braintree"),
-                .product(name: "BraintreeVenmo", package: "Braintree"),
-                .product(name: "PayPalDataCollector", package: "Braintree")
+                .product(name: "BraintreeApplePay", package: "braintree_ios"),
+                .product(name: "BraintreeCard", package: "braintree_ios"),
+                .product(name: "BraintreeCore", package: "braintree_ios"),
+                .product(name: "BraintreePaymentFlow", package: "braintree_ios"),
+                .product(name: "BraintreePayPal", package: "braintree_ios"),
+                .product(name: "BraintreeThreeDSecure", package: "braintree_ios"),
+                .product(name: "BraintreeUnionPay", package: "braintree_ios"),
+                .product(name: "BraintreeVenmo", package: "braintree_ios"),
+                .product(name: "PayPalDataCollector", package: "braintree_ios")
             ],
             exclude: ["Info.plist"],
             resources: [.copy("PrivacyInfo.xcprivacy")],
